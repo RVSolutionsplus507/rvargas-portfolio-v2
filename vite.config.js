f9850c5cadfdd8b9dddd6f9b7e0de3d26f9c6998
Roberto J. Vargas <rvargas@rv-solutions.net>
2024-10-03 19:54:12 -0500

    Arreglando vite Config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -10,5 +9,21 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]'
+      }
+    },
+    sourcemap: false,
+    minify: 'terser',
+  },
+  esbuild: {
+    jsxInject: `import React from 'react'`,
   },
 })
